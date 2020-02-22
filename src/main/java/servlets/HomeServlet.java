227e978702b2799767c597cc963cd55b746a9667
taras <taras19041@gmail.com>
2020-02-22 11:41:32 +0200

    Rename class, edit params

--- src/main/java/servlets/HomeServlet.java
+++ src/main/java/servlets/HomeServlet.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home")
-public class Home extends HttpServlet {
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
