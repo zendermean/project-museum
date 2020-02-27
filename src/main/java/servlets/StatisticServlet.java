0c3c09d5af484ac4b60b9be4788b40f7b6bddae9
taras <taras19041@gmail.com>
2020-02-27 14:35:13 +0200

    Edit Servlets

--- src/main/java/servlets/StatisticServlet.java
+++ src/main/java/servlets/StatisticServlet.java
@@ -8,10 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet("/stat")
+@WebServlet({"/stat", "/statByTechnique", "/statByMaterial"})
 public class StatisticServlet extends HttpServlet {
     private StatisticRepo statisticRepo;
 
@@ -22,21 +21,18 @@ public class StatisticServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        /*String action = req.getServletPath();
+        String action = req.getServletPath();
         switch (action) {
-            case "stat/ExhibitStatByTechnique":
+            case "/statByTechnique":
                 doGetExhibitStatByTechnique(req, resp);
                 break;
-            case "stat/ExhibitStatByMaterial":
+            case "/statByMaterial":
                 doGetExhibitStatByMaterial(req, resp);
                 break;
             default:
+                req.getRequestDispatcher("pages/AllStat.jsp").forward(req, resp);
                 break;
-        }*/
-        List<Object[]> list = statisticRepo.exhibitByTechnique();
-        req.getSession().setAttribute("list", list);
-        req.getRequestDispatcher("statByTechnique.jsp").forward(req, resp);
-
+        }
     }
 
     @Override
@@ -47,12 +43,15 @@ public class StatisticServlet extends HttpServlet {
     protected void doGetExhibitStatByTechnique(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Object[]> list = statisticRepo.exhibitByTechnique();
         req.getSession().setAttribute("list", list);
-        req.getRequestDispatcher("statByTechnique.jsp").forward(req, resp);
+        req.getSession().setAttribute("name", "Technique");
+        req.getRequestDispatcher("pages/statBy.jsp").forward(req, resp);
     }
 
     protected void doGetExhibitStatByMaterial(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Object[]> list = statisticRepo.exhibitByMaterial();
         req.getSession().setAttribute("list", list);
-        req.getRequestDispatcher("statByMaterial.jsp").forward(req, resp);
+        req.getSession().setAttribute("name", "Material");
+        req.getRequestDispatcher("pages/statBy.jsp").forward(req, resp);
     }
+
 }
